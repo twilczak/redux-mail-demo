0395424ecc4c8f06db0c785ebeb5692a8ca808dc
Tom Wilczak <tom.wilczak@gmail.com>
2018-04-30 16:38:33 -0500

    Add store & provider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-import { Mailbox } from "./Mailbox/Mailbox";
+import { Provider } from 'react-redux';
+import { Mailbox } from './Mailbox/Mailbox';
+import { Store } from './Store';
 
 import './App.css';
 
 const App = () => {
     return (
-        <div className="App">
-            <Switch>
-                <Route exact path="/" render={() =>
-                    <Redirect to="/inbox"/>
-                }/>
-                <Route path="/inbox" component={Mailbox}/>
-                <Route path="/outbox" component={Mailbox}/>
-            </Switch>
-        </div>
+        <Provider store={Store}>
+            <div className="App">
+                <Switch>
+                    <Route exact path="/" render={() =>
+                        <Redirect to="/inbox"/>
+                    }/>
+                    <Route path="/inbox" component={Mailbox}/>
+                    <Route path="/outbox" component={Mailbox}/>
+                </Switch>
+            </div>
+        </Provider>
     );
 };
 
