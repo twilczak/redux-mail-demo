95c6573770b346ecad33402018b6d07b9bdff0a6
Tom Wilczak <tom.wilczak@gmail.com>
2018-05-02 16:12:57 -0500

    Add actions for deleting messages

--- src/MessageReader/MessageReaderActions.js
+++ src/MessageReader/MessageReaderActions.js
@@ -4,11 +4,17 @@ import { hostUrl } from '../constants';
 export const FETCH_MESSAGE = 'FETCH_MESSAGE';
 export const FETCH_MESSAGE_SUCCESS = 'FETCH_MESSAGE_SUCCESS';
 export const FETCH_MESSAGE_FAILED = 'FETCH_MESSAGE_FAILED';
+export const DELETE_MESSAGE = 'DELETE_MESSAGE';
+export const DELETE_MESSAGE_SUCCESS = 'DELETE_MESSAGE_SUCCESS';
+export const DELETE_MESSAGE_FAILED = 'DELETE_MESSAGE_FAILED';
 
 export const messageReaderActions = {
     FETCH_MESSAGE,
     FETCH_MESSAGE_FAILED,
     FETCH_MESSAGE_SUCCESS,
+    DELETE_MESSAGE,
+    DELETE_MESSAGE_FAILED,
+    DELETE_MESSAGE_SUCCESS,
 };
 
 export const fetchMessage = (mailbox, messageId) => dispatch => {
@@ -25,5 +31,21 @@ export const fetchMessage = (mailbox, messageId) => dispatch => {
         })
         .catch(error => {
             dispatch({type: FETCH_MESSAGE_FAILED, error, messageId});
+        });
+};
+
+export const deleteMessage = (mailbox, messageId) => dispatch => {
+    const parsedMailbox = mailbox.split('/').join('');
+    const url = `${this.hostUrl}/${parsedMailbox}/${messageId}`;
+
+    dispatch({type: DELETE_MESSAGE, mailbox: parsedMailbox, messageId});
+
+    fetch(url, {method: 'DELETE'})
+        .then(response => response.json())
+        .then(response => {
+            dispatch({type: DELETE_MESSAGE_SUCCESS, mailbox: parsedMailbox, messageId})
+        })
+        .catch(error => {
+            dispatch({type: DELETE_MESSAGE_FAILED, mailbox: parsedMailbox, error, messageId});
         });
 };
