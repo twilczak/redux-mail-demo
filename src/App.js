3f5408d7d6b8a93ab3feaaff367147dfa65c9511
Tom Wilczak <tom.wilczak@gmail.com>
2018-05-07 16:00:15 -0500

    Move provider and history infrastructure from app.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'react-router-redux';
 
-import { History } from './History';
 import { Mailbox } from './Mailbox/Mailbox';
-import { Store } from './Store';
+
 
 import './App.css';
 
 const App = () => {
     return (
-        <Provider store={Store}>
-            <ConnectedRouter history={History}>
-                <div className="App">
-                    <Switch>
-                        <Route exact path="/" render={() =>
-                            <Redirect to="/inbox"/>
-                        }/>
-                        <Route path="/inbox" component={Mailbox}/>
-                        <Route path="/outbox" component={Mailbox}/>
-                    </Switch>
-                </div>
-            </ConnectedRouter>
-        </Provider>
+        <div className="App">
+            <Switch>
+                <Route exact path="/" render={() =>
+                    <Redirect to="/inbox"/>
+                }/>
+                <Route path="/inbox" component={Mailbox}/>
+                <Route path="/outbox" component={Mailbox}/>
+            </Switch>
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import registerServiceWorker from './registerServiceWorker';
 
 import App from './App';
+import { History } from './History';
+import { Store } from './Store';
 
 import './index.css';
 
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render((<App/>), document.getElementById('root'));
+ReactDOM.render((
+    <Provider store={Store}>
+        <ConnectedRouter history={History}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>
+), document.getElementById('root'));
 
 registerServiceWorker();
